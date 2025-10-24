d33f0916eedec1118c5f4b5a5e464664cee5600c
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2025-10-25 00:11:57 +0500

    Added Insertion Sort

---
+++ Ch4_Sorting_Algo/insertion_sort_l10.py
@@ -0,0 +1,14 @@
+
+def insertion_sort(nums):
+   
+   for i in range(1,len(nums)):
+      j = i
+      while j > 0 and nums[j-1] > nums[j]:
+         nums[j], nums[j-1] = nums[j-1], nums[j]
+         j -= 1
+   print(nums)
+   return nums
+
+
+
+insertion_sort([5,2,65,2,4,89])
